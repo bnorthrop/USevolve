444eae7a172748ca80f6dfced50e730070b08094
Blake Northrop <bsnorthrop@wesleyan.edu>
2020-11-25 11:34:46 -0500

    require inside functions

--- R/3 state_winner_func.R
+++ R/3 state_winner_func.R
@@ -1,6 +1,3 @@
-require(dplyr)
-require(ggplot2)
-require(maps)
 
 state_map <- read.csv("/Users/blakenorthrop/Desktop/QAC356/Final Project Prep/Map Data/state_map.csv")
 
@@ -10,27 +7,30 @@ statepres <- read.csv("/Users/blakenorthrop/Desktop/QAC356/Final Project Prep/El
 
 # Create function
 state_winner <- function(Year=2016, states=c()){
+  require(dplyr)
+  require(ggplot2)
+  require(maps)
   state_election_years <- c(1976, 1980, 1984, 1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016)
   if(Year %in% state_election_years){
-    
+
     statepres$party[statepres$party != "democrat" & statepres$party != "republican"] <- "other"
     partycolor <- c("blue2", "red1", "yellow")
     names(partycolor) <- unique(statepres$party)
     ############
     if(length(states)==0){
-      state_majority <- statepres %>% 
-        filter(year==Year) %>% 
-        group_by(region) %>% 
+      state_majority <- statepres %>%
+        filter(year==Year) %>%
+        group_by(region) %>%
         slice_max(state_percent)
     } else{
-      state_map <- state_map %>% 
+      state_map <- state_map %>%
         filter(region %in% states)
-      state_majority <- statepres %>% 
-        filter(region %in% states, year==Year) %>% 
-        group_by(region) %>% 
+      state_majority <- statepres %>%
+        filter(region %in% states, year==Year) %>%
+        group_by(region) %>%
         slice_max(state_percent)
     }
-    
+
     ggplot()+
       geom_map(data=state_map, aes(map_id= region), map = state_map) +
       geom_map(data=state_majority, aes(map_id= region, fill=party), map = state_map) +
@@ -38,7 +38,7 @@ state_winner <- function(Year=2016, states=c()){
       scale_fill_manual(name = "party", values = partycolor) +
       coord_map("albers", lat0=30, lat1=40) +
       ggtitle("State Winner Map")
-  } 
+  }
   else if(Year == 2020){
     stop("Data not yet available")
   }
--- R/change_func.R
+++ R/change_func.R
@@ -1,5 +1,3 @@
-library(dplyr)
-library(ggplot2)
 
 covid <- read.csv("/Users/blakenorthrop/Desktop/QAC356/finalpackageprep/Data/US Covid (to Nov8).csv")
 
@@ -13,11 +11,13 @@ covid$date <- as.Date(covid$date, format = "%Y-%m-%d")
 
 # Make function
 percent_change <- function(data=covid, state=c()){
-  filtered_data <- data %>% 
-    filter(region %in% state) %>% 
-    group_by(new_case, date)  
+  library(dplyr)
+  library(ggplot2)
+  filtered_data <- data %>%
+    filter(region %in% state) %>%
+    group_by(new_case, date)
    # mutate(change = new_case[date] - new_case[date-1])
-  
+
   # return(filtered_data$change)
 
 }
